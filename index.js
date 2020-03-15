160b3361b065b9f43682bfabe561df802f4cbce8
Khor Li Chuan <billgatepls@hotmail.com>
2020-03-15 14:09:36 +0800

    ver 1.0.0

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname , "public")))
+
+
+//listen to port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , ()=>console.log(`Page running on port : ${PORT}`));
